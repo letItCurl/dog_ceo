ba8af21905391d90ff096365f22d32f3e5b7da71
letItCurl <rolandlopez.developer@gmail.com>
2024-01-16 07:38:54 +0700

    scaffold turbo frame.

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,4 +1,8 @@
 class BreedsController < ApplicationController
   def show
   end
+
+  def update
+    render turbo_stream: turbo_stream.replace("dog-image", partial: "breeds/dog-image", locals: { url: "https://images.dog.ceo/breeds/terrier-yorkshire/n02094433_6338.jpg" })
+  end
 end
